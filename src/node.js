291833f780f09d812080a76977385bc75f319791
Nirmal Subedi <101914003+NirmalSubedi@users.noreply.github.com>
2026-04-13 16:58:30 -0700

    Add 'Node' constructor function

---
+++ src/node.js
@@ -0,0 +1,15 @@
+export class Node {
+  constructor(data, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+
+  setLeft(value) {
+    this.left = value;
+  }
+
+  setRight(value) {
+    this.right = value;
+  }
+}
